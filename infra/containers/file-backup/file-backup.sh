2124902a73e836815fc34a0e87b7ab26b945ff9d
Ben Naylor <nayben@gmail.com>
2025-10-28 14:18:56 +0100

    Backups for docmost + nocodb (#152)
    Add some backup cronjobs for nocodb and docmost!
    
    These are kinda rough/ready backups. Both nocodb and docmost have a
    storage directory (for images etc), and also of course data in their
    respective database.
    
    How does the restore process work? Yes
    Where are the backups stored? On a hetzner storage box (another ~3.5
    eur/month)
    How can we extract the backups if we ever need them? At present, I'm the
    only one with the keys here. I'll do some thinking about the best way to
    share this. (see below)
    Why did we need new containers? I couldn't find any open/maintained
    images that had both psql + ssh. It would be nice to not have to manage
    some containers, but not the end of the world. Could also manage this
    automagically with github actions in theory.
    
    Storage box file structure:
    
    ```
    |   # root user (me)
    \- k3s    # sub-1
       |- nocodb   # sub-2
       |   \- backups
       |       |- pg_backup_<date>.tar.gz
       |       \- file_backup_<date>.tar.gz
       \- docmost   # sub-3
           \- backups
               |- pg_backup_<date>.tar.gz
               \- file_backup_<date>.tar.gz
    ```
    
    :point_up: Each individual service (docmost/nocodb) has it's own set of
    ssh keys to access the storage box. This means if one service gets
    pwned, at least it doesn't pwn the other. The good news is that I had
    some foresight to have a separate account for the `k3s` directory, which
    means we could create some ssh keys there, and share them in the keepass
    for emergency access etc.

---
+++ infra/containers/file-backup/file-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# Setup the SSH connection
+ssh_setup
+
+file_path=${FILE_PATH:-""}
+if [ -z "$file_path" ]; then
+  lerror "FILE_PATH is not set. Exiting."
+fi
+if [ ! -e "$file_path" ]; then
+  lerror "File $file_path does not exist. Exiting."
+fi
+
+# Backup process
+backup_dir="/tmp"
+backup_file="file_backup.sql.gz"
+backup_path="${backup_dir}/${backup_file}"
+
+tar -czf $backup_path $file_path
+
+file_info=$(ls -lh "$backup_path")
+ldebug "Backup file at $backup_path info: $file_info"
+
+ssh_dir=${SSH_REMOTE_DIR:-""}
+ssh_send_file "$backup_path" "$ssh_dir"
