ab59847f01c2f3179f87cec3d2fb0c376d80ed4b
Ben Naylor <nayben@gmail.com>
2026-06-03 21:56:14 +0200

    Prettier was mad, now it's happy

--- assets/js/pristine.js
+++ assets/js/pristine.js
@@ -1,7 +1,14 @@
 // Add missing URL regex
-URL_REGEX = /^(https?:\/\/)?[a-zA-Z0-9@:%._\+~#=\-]{1,256}\.[a-zA-Z0-9\(\)]{1,63}\b(?:[a-zA-Z0-9\(\)@:%_\+.~#?&\/=]*)$/
-Pristine.addValidator('url', (val) => !val || URL_REGEX.test(val), pristineValidationMessages.url, 1, false); 
+URL_REGEX =
+  /^(https?:\/\/)?[a-zA-Z0-9@:%._\+~#=\-]{1,256}\.[a-zA-Z0-9\(\)]{1,63}\b(?:[a-zA-Z0-9\(\)@:%_\+.~#?&\/=]*)$/;
+Pristine.addValidator(
+  "url",
+  (val) => !val || URL_REGEX.test(val),
+  pristineValidationMessages.url,
+  1,
+  false
+);
 
 // Add the localised messages. Values for pristineValidationMessages are added to the head element
-Pristine.addMessages("{{ .Lang }}", pristineValidationMessages)
-Pristine.setLocale("{{ .Lang }}")
+Pristine.addMessages("{{ .Lang }}", pristineValidationMessages);
+Pristine.setLocale("{{ .Lang }}");
